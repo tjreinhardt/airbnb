76d3ef83c3dd28fc327a8a950b581f0affc21a3b
Tim Reinhardt <timreinhardtmusic@gmail.com>
2022-10-25 15:04:03 -0700

    deploy fix

--- airbnb-clone/components/SmallCard.js
+++ airbnb-clone/components/SmallCard.js
@@ -6,7 +6,7 @@ function SmallCard({ img, location, distance }) {
     <div className="flex items-center m-2 mt-5 space-x-4 rounded-xl cursor-pointer hover:bg-gray-100 hover:scale-105 transition transform duration-200 ease-out">
       {/* Left */}
       <div className='relative h-16 w-16'>
-        <Image src={img} layout="fill" className='rounded-lg' />
+        <Image src={img} layout="fill" className='rounded-lg' alt="" />
       </div>
       {/* Right */}
 
